edae1a2282f3e8949e04a4da865a37e804035e37
Torsten Bögershausen <torsten.bogershausen@esss.se>
2023-12-20 13:54:55 +0100

    921: Next

--- test/pytests36/921_Simulator_HLS_LLS_no_move.py
+++ test/pytests36/921_Simulator_HLS_LLS_no_move.py
@@ -25,8 +25,9 @@ class Test(unittest.TestCase):
 
     axisCom = AxisCom(url_string, log_debug=False)
     axisMr = AxisMr(axisCom)
-    old_HLM = axisCom.get(".HLM")
-    old_LLM = axisCom.get(".LLM")
+    old_PwrAuto = axisCom.get("-PwrAuto")
+    old_DHLM = axisCom.get("-CfgDHLM")
+    old_DLLM = axisCom.get("-CfgDLLM")
 
     print(f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam}")
 
@@ -34,18 +35,26 @@ class Test(unittest.TestCase):
     def test_TC_92101(self):
         tc_no = tc_no_base + 1
         self.axisCom.putDbgStrToLOG("Start " + str(int(tc_no)), wait=True)
+        self.axisCom.put("-PwrAuto", 0)
+        self.axisMr.setValueOnSimulator(tc_no, "bAxisHomed", 1)
         self.axisMr.powerOnHomeAxis(tc_no)
+        # self.axisMr.setValueOnSimulator(tc_no, "fHighHardLimitPos", self.old_HLM)
+        # self.axisMr.setValueOnSimulator(tc_no, "fLowHardLimitPos", self.old_LLM)
         self.axisCom.putDbgStrToLOG("Passed " + str(tc_no), wait=True)
 
     # low limit switch
     def test_TC_92102(self):
         tc_no = tc_no_base + 2
         self.axisCom.putDbgStrToLOG("Start " + str(int(tc_no)), wait=True)
-        self.axisMr.setSoftLimitsOff(tc_no, disableDHLM=False)
-        self.axisMr.moveWait(tc_no, self.old_LLM)
+        # self.axisMr.setSoftLimitsOff(tc_no, disableDHLM=False)
+        self.axisMr.setSoftLimitsOff(tc_no)
+        self.axisMr.moveWait(tc_no, self.old_DLLM)
         self.axisCom.put(".JOGR", 1)
         wait_for_done = 60
         self.axisMr.waitForStartAndDone(tc_no, wait_for_done)
+        rbv = self.axisCom.get(".RBV", use_monitor=False)
+        self.axisMr.setSoftLimitsOn(tc_no, low_limit=rbv, high_limit=self.old_DHLM)
+        self.axisMr.setSoftLimitsOff(tc_no, disableDLLM=False)
         lls = self.axisCom.get(".LLS", use_monitor=False)
         passed = lls
         if passed:
@@ -59,7 +68,9 @@ class Test(unittest.TestCase):
         print(
             f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} {filnam}:{lineno()} {tc_no} teardown_class"
         )
-        self.axisMr.setSoftLimitsOn(
-            tc_no, low_limit=self.old_LLM, high_limit=self.old_HLM
-        )
+        if False:
+            self.axisMr.setSoftLimitsOn(
+                tc_no, low_limit=self.old_LLM, high_limit=self.old_HLM
+            )
+            self.axisCom.put("-PwrAuto", self.old_PwrAuto)
         self.axisCom.close()
